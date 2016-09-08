8a30e50a0dbcf6e3dc4684de0a123c810d2db9fe
mort786f <mort786f@edu.eal.dk>
2016-09-08 12:33:36 +0200

    N/A

--- 02_Bottles/Song.cs
+++ 02_Bottles/Song.cs
@@ -6,7 +6,56 @@ namespace _02_Bottles
     {
         internal string CountBottles(int v)
         {
-            return ;
+            if (v == 3)
+            {
+                return "3 bottles of beer on the wall."
+                + " 3 bottles of beer."
+                + " Take one down and pass it around."
+                + " 2 bottles of beer on the wall."
+                + " 2 bottles of beer on the wall."
+                + " 2 bottles of beer."
+                + " Take one down and pass it around."
+                + " 1 bottle of beer on the wall."
+                + " 1 bottle of beer on the wall."
+                + " 1 bottle of beer."
+                + " Take one down and pass it around."
+                + " No more bottles of beer on the wall.";
+            }
+            else
+            {
+                if (v == 2)
+                {
+                    return "2 bottles of beer on the wall."
+                + " 2 bottles of beer."
+                + " Take one down and pass it around."
+                + " 1 bottle of beer on the wall."
+                + " 1 bottle of beer on the wall."
+                + " 1 bottle of beer."
+                + " Take one down and pass it around."
+                + " No more bottles of beer on the wall.";
+                }
+                else
+                {
+                    if (v == 1)
+                    {
+                        return "1 bottle of beer on the wall."
+                            + " 1 bottle of beer."
+                            + " Take one down and pass it around."
+                            + " No more bottles of beer on the wall.";
+                    }
+                    else
+                    {
+                        if (v == 0)
+                        {
+                            return " No more bottles of beer on the wall.";
+                        }
+                        else
+                        {
+                            return "Song Over";
+                        }
+                    }
+                }
+            }
         }
     }
 }
